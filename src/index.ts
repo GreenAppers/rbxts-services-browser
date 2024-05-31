c9b3c4aa3231bb837e197fdf6153fbe4cb295396
GreenAppers <GreenAppers@gmail.com>
2024-05-30 19:39:40 -0700

    Add a few Instance properties

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,30 @@
-export const Players: Record<string, any> = {}
+export interface Instance extends Record<string, any> {
+  FindFirstChild<X>(name: string): X | undefined;
+  Name: string
+  Parent?: Instance
+  Destroy: () => void
+}
 
-export const RunService: Record<string, any> = {}
+export const Players: Instance = {
+  FindFirstChild: (name) => undefined,
+  Name: 'Players',
+  Destroy: () => {},
+}
 
-export const UserInputService: Record<string, any> = {}
+export const RunService: Instance = {
+  FindFirstChild: (name) => undefined,
+  Name: 'RunService',
+  Destroy: () => {},
+}
+
+export const SoundService: Instance = {
+  FindFirstChild: (name) => undefined,
+  Name: 'SoundService',
+  Destroy: () => {},
+}
+
+export const UserInputService: Instance = {
+  FindFirstChild: (name) => undefined,
+  Name: 'UserInputService',
+  Destroy: () => {},
+}
